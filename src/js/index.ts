30535ec629f98a0ff13c52512f51ae069e9df7f8
dustin-jw <dustin@heysparkbox.com>
2022-02-04 08:48:27 -0700

    feat: set up some minimal TS files to start with

---
+++ src/js/index.ts
@@ -0,0 +1,9 @@
+import add from './utilities/add';
+import subtract from './utilities/subtract';
+import multiply from './utilities/multiply';
+import divide from './utilities/divide';
+
+console.log(`2 + 2 is ${add(2, 2)}`);
+console.log(`2 - 2 is ${subtract(2, 2)}`);
+console.log(`2 * 2 is ${multiply(2, 2)}`);
+console.log(`2 / 2 is ${divide(2, 2)}`);
