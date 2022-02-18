3cd0e9781a2ef317266f89692c91d6e3fff83c8d
dustin-jw <dustin@heysparkbox.com>
2022-02-18 10:49:20 -0700

    test: beef up math.ts test coverage

--- src/pages/math.test.ts
+++ src/pages/math.test.ts
@@ -1,4 +1,4 @@
-import { mathPageContent } from '.';
+import { math, mathPageContent } from '.';
 
 const additionContent = `
   <h1>Add</h1>
@@ -33,19 +33,67 @@ const divisionContent = `
 `;
 
 describe('math page', () => {
-  it('renders the "add" page correctly', () => {
-    expect(mathPageContent('Add', '2 + 2 = 4')).toEqual(additionContent);
+  describe('mathPageContent', () => {
+    it('renders the "add" page content correctly', () => {
+      expect(mathPageContent('Add', '2 + 2 = 4')).toEqual(additionContent);
+    });
+
+    it('renders the "subtract" page content correctly', () => {
+      expect(mathPageContent('Subtract', '2 - 2 = 0')).toEqual(subtractionContent);
+    });
+
+    it('renders the "multiply" page content correctly', () => {
+      expect(mathPageContent('Multiply', '2 &times; 2 = 4')).toEqual(multiplicationContent);
+    });
+
+    it('renders the "divide" page content correctly', () => {
+      expect(mathPageContent('Divide', '2 &divide; 2 = 1')).toEqual(divisionContent);
+    });
   });
 
-  it('renders the "subtract" page correctly', () => {
-    expect(mathPageContent('Subtract', '2 - 2 = 0')).toEqual(subtractionContent);
-  });
+  describe('math', () => {
+    it('renders the "add" page correctly', () => {
+      document.documentElement.innerHTML = math('add', '2 + 2 = 4');
+      const main = document.querySelector('main');
+      const title = document.querySelector('title');
+      const description = document.querySelector('meta[name="description"]');
 
-  it('renders the "multiply" page correctly', () => {
-    expect(mathPageContent('Multiply', '2 &times; 2 = 4')).toEqual(multiplicationContent);
-  });
+      expect(main?.innerHTML.includes(additionContent)).toEqual(true);
+      expect(title?.innerHTML).toEqual('Add | Cohort Project Starter');
+      expect(description?.getAttribute('content')).toEqual('This is the math page. View the results of sums, subtractions, multiplications, and divisions here.');
+    });
 
-  it('renders the "divide" page correctly', () => {
-    expect(mathPageContent('Divide', '2 &divide; 2 = 1')).toEqual(divisionContent);
+    it('renders the "subtract" page correctly', () => {
+      document.documentElement.innerHTML = math('subtract', '2 - 2 = 0');
+      const main = document.querySelector('main');
+      const title = document.querySelector('title');
+      const description = document.querySelector('meta[name="description"]');
+
+      expect(main?.innerHTML.includes(subtractionContent)).toEqual(true);
+      expect(title?.innerHTML).toEqual('Subtract | Cohort Project Starter');
+      expect(description?.getAttribute('content')).toEqual('This is the math page. View the results of sums, subtractions, multiplications, and divisions here.');
+    });
+
+    it('renders the "multiply" page correctly', () => {
+      document.documentElement.innerHTML = math('multiply', '2 &times; 2 = 4');
+      const main = document.querySelector('main');
+      const title = document.querySelector('title');
+      const description = document.querySelector('meta[name="description"]');
+
+      expect(main?.innerHTML.includes(multiplicationContent.replace('&times;', '×'))).toEqual(true);
+      expect(title?.innerHTML).toEqual('Multiply | Cohort Project Starter');
+      expect(description?.getAttribute('content')).toEqual('This is the math page. View the results of sums, subtractions, multiplications, and divisions here.');
+    });
+
+    it('renders the "divide" page correctly', () => {
+      document.documentElement.innerHTML = math('divide', '2 &divide; 2 = 1');
+      const main = document.querySelector('main');
+      const title = document.querySelector('title');
+      const description = document.querySelector('meta[name="description"]');
+
+      expect(main?.innerHTML.includes(divisionContent.replace('&divide;', '÷'))).toEqual(true);
+      expect(title?.innerHTML).toEqual('Divide | Cohort Project Starter');
+      expect(description?.getAttribute('content')).toEqual('This is the math page. View the results of sums, subtractions, multiplications, and divisions here.');
+    });
   });
 });
